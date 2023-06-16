732af79eb56fab087e1df62160e3a7cf6f41ae6e
leroy <leroybacky@gmail.com>
2023-06-16 18:01:09 +0200

    pdf generation finish

--- mafia-front/src/pages/ReportsPage.js
+++ mafia-front/src/pages/ReportsPage.js
@@ -4,6 +4,9 @@ import {Link} from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import * as Icon from "react-bootstrap-icons";
 
 export default function ReportsPage() {
   const [reports, setReports] = useState([]);
@@ -30,25 +33,39 @@ export default function ReportsPage() {
   }
   
   return (
-    <div>
+    <div style={{marginBottom: 15, marginTop: 15}}>
       <h1 style={{textAlign: "center"}}>Reports</h1>
 
       <Form className="mt-4">
-        <Form.Control type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)}>
-        </Form.Control>
-        <Form.Control type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}>
-        </Form.Control>
-        <Button 
-          onClick={generatePdf}
-          disabled={!(startDate && startDate !== '' && endDate && endDate !== '')}
-        >
-          generate
-        </Button>
+        
+        <Row style={{textAlign: "center"}}>
+          <Col>
+            <Form.Control type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)}>
+            </Form.Control>
+          </Col>
+          <Col>
+            <Form.Control type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}>
+            </Form.Control>
+          </Col>
+          <Col>
+            <Button
+              style={{width: "100%"}}
+              onClick={generatePdf}
+              disabled={!(startDate && startDate !== '' && endDate && endDate !== '')}
+            >
+              generate
+            </Button>
+          </Col>
+         
+        </Row>
       </Form>
       
       {reports.map(rep => (
-        <Card key={rep}>
-          <Link to={"http://localhost:8080/api/report/download/" + rep}>{rep}</Link>
+        <Card style={{marginTop: 10}} key={rep}>
+          <Card.Body>
+            <Icon.FileEarmarkPlus size={30} style={{marginRight: 15}}/>
+            <Link style={{textDecoration: "none"}} to={"http://localhost:8080/api/report/download/" + rep}>{rep}</Link>
+          </Card.Body>
         </Card>
       ))}
     </div>
